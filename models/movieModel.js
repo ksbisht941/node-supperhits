dd52c1c0bf950c0ea0903eea3eeccb2d2ee11954
ksbisht <ksbisht941@gmail.com>
2021-09-20 23:59:35 +0530

    query middleware

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,112 +1,128 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
-const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "A movie must have a title."],
-  },
-  slug: String,
-  rated: {
-    type: String,
-    required: [true, "A movie must have a rated."],
-    enum: {
-      values: [
-        "G",
-        "PG",
-        "PG-13",
-        "R",
-        "NC-17",
-        "TV-Y",
-        "TV-Y7",
-        "TV-Y7 FV",
-        "TV-G",
-        "TV-PG",
-        "TV-14",
-        "TV-MA",
-        "Approved",
-      ],
-      message: "Invalid movie rated.",
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "A movie must have a title."],
     },
-  },
-  released: {
-    type: String,
-    required: [true, "A movie must have a released year."],
-  },
-  runtime: {
-    type: String,
-    required: [true, "A movie must have a runtime."],
-  },
-  genre: {
-    type: [String],
-    required: [true, "A movie must have a runtime."],
-    enum: {
-      values: [
-        "Action",
-        "Adventure",
-        "Comedy",
-        "Crime",
-        "Mystery",
-        "Fantasy",
-        "Historical",
-        "Horror",
-        "Romance",
-        "Satire",
-        "Biography",
-        "Drama",
-      ],
-      message: ["Invalid movie genre"],
+    slug: String,
+    rated: {
+      type: String,
+      required: [true, "A movie must have a rated."],
+      enum: {
+        values: [
+          "G",
+          "PG",
+          "PG-13",
+          "R",
+          "NC-17",
+          "TV-Y",
+          "TV-Y7",
+          "TV-Y7 FV",
+          "TV-G",
+          "TV-PG",
+          "TV-14",
+          "TV-MA",
+          "Approved",
+        ],
+        message: "Invalid movie rated.",
+      },
     },
-  },
-  popularity: {
-    type: Number,
-    default: 10,
-    min: [1, "Popularity must be above 1"],
-    max: [100, "Popularity must be below 100"],
-    set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
-  },
-  voters: {
-    type: Number,
-    default: 5,
-  },
-  director: {
-    type: [String],
-    required: [true, "A movie must have a runtime."],
-  },
-  plot: {
-    type: String,
-    required: [true, "A movie must have a runtime."],
-  },
-  actors: {
-    type: [String],
-    required: [true, "A movie must have a runtime."],
-  },
-  writers: {
-    type: [String],
-    required: [true, "A movie must have a runtime."],
-  },
-  sequal: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "Movie",
+    released: {
+      type: String,
+      required: [true, "A movie must have a released year."],
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
+    runtime: {
+      type: String,
+      required: [true, "A movie must have a runtime."],
+    },
+    genre: {
+      type: [String],
+      required: [true, "A movie must have a runtime."],
+      enum: {
+        values: [
+          "Action",
+          "Adventure",
+          "Comedy",
+          "Crime",
+          "Mystery",
+          "Fantasy",
+          "Historical",
+          "Horror",
+          "Romance",
+          "Satire",
+          "Biography",
+          "Drama",
+        ],
+        message: ["Invalid movie genre"],
+      },
+    },
+    popularity: {
+      type: Number,
+      default: 10,
+      min: [1, "Popularity must be above 1"],
+      max: [100, "Popularity must be below 100"],
+      set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
+    },
+    voters: {
+      type: Number,
+      default: 5,
+    },
+    director: {
+      type: [String],
+      required: [true, "A movie must have a runtime."],
+    },
+    plot: {
+      type: String,
+      required: [true, "A movie must have a runtime."],
+    },
+    actors: {
+      type: [String],
+      required: [true, "A movie must have a runtime."],
+    },
+    writers: {
+      type: [String],
+      required: [true, "A movie must have a runtime."],
+    },
+    sequal: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Movie",
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    }
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-}, {
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
+);
+
+movieSchema.virtual("adult").get(function () {
+  return this.rated === 'R' ? true : false;
 });
 
-movieSchema.virtual("fieldName").get(function () {
-  return "null";
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+movieSchema.pre('save', function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+// QUERY DOCUMENTS
+movieSchema.pre('find', function(next) {
+  this.find( { rated: {$ne: 'R'} });
+  next();
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
