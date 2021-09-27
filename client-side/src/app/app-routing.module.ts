ac35daf5ccf7dc9ff3b018796b7a6fc47b289c93
ksbisht <ksbisht941@gmail.com>
2021-09-27 12:09:57 +0530

    structure client side project

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'in', pathMatch: 'full' },
+  {
+    path: 'in',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'in/movies',
+    loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule)
+  },
+  // {
+  //   path: 'in/movies',
+  //   loadChildren: () => import('./modules/movies/movies.module').then(m => m.MoviesModule)
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
